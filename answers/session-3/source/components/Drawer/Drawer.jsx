12c6e9614e50a20659db2da38822962973fc5242
Jahed Ahmed <jahed.public@gmail.com>
2017-05-02 12:25:13 +0100

    Added drawer navigation.

--- answers/session-3/source/components/Drawer/Drawer.jsx
+++ answers/session-3/source/components/Drawer/Drawer.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Drawer.scss'
 
-export default function Drawer({ active }) {
+function DrawerNavigationItem({ href, onClick, children }) {
+    return (
+        <li className="Drawer__Navigation__Item">
+            <a className="Drawer__Navigation__Item__Link" href={href} onClick={onClick}>
+                {children}
+            </a>
+        </li>
+    )
+}
+
+DrawerNavigationItem.propTypes = {
+    href: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+    onClick: PropTypes.func
+}
+
+DrawerNavigationItem.defaultProps = {
+    onClick: () => undefined
+}
+
+function DrawerNavigation({ children }) {
+    return (
+        <ul className="Drawer__Navigation">
+            {children}
+        </ul>
+    )
+}
+
+DrawerNavigation.propTypes = {
+    children: PropTypes.node.isRequired
+}
+
+export default function Drawer({ active, onInteraction }) {
     const classes = ['Drawer']
     if(active) {
         classes.push('Drawer--active')
@@ -10,15 +42,21 @@ export default function Drawer({ active }) {
 
     return (
         <div className={classes.join(' ')}>
-            content
+            <DrawerNavigation>
+                <DrawerNavigationItem href="#" onClick={onInteraction}>Intro</DrawerNavigationItem>
+                <DrawerNavigationItem href="#dominion"  onClick={onInteraction}>Dominion</DrawerNavigationItem>
+                <DrawerNavigationItem href="#serenity"  onClick={onInteraction}>Serenity</DrawerNavigationItem>
+            </DrawerNavigation>
         </div>
     )
 }
 
 Drawer.propTypes = {
-    active: PropTypes.bool
+    active: PropTypes.bool,
+    onInteraction: PropTypes.func
 }
 
 Drawer.defaultProps = {
-    active: false
+    active: false,
+    onInteraction: () => undefined
 }
--- answers/session-3/source/components/Drawer/index.jsx
+++ answers/session-3/source/components/Drawer/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { hideDrawer } from '~/actions/drawer'
 import Drawer from './Drawer'
 
 export default connect(
-    state => ({ active: state.drawer.show })
+    state => ({
+        active: state.drawer.show
+    }),
+    dispatch => ({
+        onInteraction() {
+            dispatch(hideDrawer())
+        }
+    })
 )(Drawer)
